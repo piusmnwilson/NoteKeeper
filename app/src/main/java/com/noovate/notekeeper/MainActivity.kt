cf462c8c542a321e174314de0844e70a0daeb210
Ň¥ΔǤΔĦƗΜΔ <piusmnwilson@gmail.com>
2020-12-22 02:20:18 +0300

    built and ran app

--- app/src/main/java/com/noovate/notekeeper/MainActivity.kt
+++ app/src/main/java/com/noovate/notekeeper/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        //menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            //R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
