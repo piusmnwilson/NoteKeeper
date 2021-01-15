1a5d9bf57ce3090b0c3c01eda4206053fef4decb
Ň¥ΔǤΔĦƗΜΔ <piusmnwilson@gmail.com>
2021-01-15 14:55:18 +0300

    Building your first app: Building App with Kotlin_describing types with Kotlin.

--- app/src/main/java/com/noovate/notekeeper/MainActivity.kt
+++ app/src/main/java/com/noovate/notekeeper/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.noovate.notekeeper
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
