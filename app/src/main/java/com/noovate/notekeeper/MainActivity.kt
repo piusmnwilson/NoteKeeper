d3087a290c261cb5d1dbdb85c38ced6651c936ec
Ň¥ΔǤΔĦƗΜΔ <piusmnwilson@gmail.com>
2021-01-15 23:01:57 +0300

    deleted fab and textDisplayedValue code from MainActivity.kt

--- app/src/main/java/com/noovate/notekeeper/MainActivity.kt
+++ app/src/main/java/com/noovate/notekeeper/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.noovate.notekeeper
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -13,14 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            val originalValue = textDisplayedValue.text.toString().toInt()
-            val newValue = originalValue * 2
-            textDisplayedValue.text = newValue.toString()
-            Snackbar.make(view, "Value $originalValue changed to $newValue",
-                    Snackbar.LENGTH_LONG)
-                    .show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
